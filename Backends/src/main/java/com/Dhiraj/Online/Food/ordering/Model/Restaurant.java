22e1fba2c1f1815504e76043a8034cae97ccfc7d
Dhiraj-231 <dk194945@gmail.com>
2024-12-22 18:10:36 +0530

    "Model Creation for the food delivery app"

---
+++ Backends/src/main/java/com/Dhiraj/Online/Food/ordering/Model/Restaurant.java
@@ -0,0 +1,19 @@
+package com.Dhiraj.Online.Food.ordering.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
