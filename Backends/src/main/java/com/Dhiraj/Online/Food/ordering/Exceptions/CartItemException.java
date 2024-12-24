7412c39d0f8206aff7d6f69ae0c86d3e3d7bcb6c
Dhiraj-231 <dk194945@gmail.com>
2024-12-24 11:34:34 +0530

    "CartItem and Cart Service Implemention"

---
+++ Backends/src/main/java/com/Dhiraj/Online/Food/ordering/Exceptions/CartItemException.java
@@ -0,0 +1,9 @@
+package com.Dhiraj.Online.Food.ordering.Exceptions;
+
+public class CartItemException extends Exception {
+
+    public CartItemException(String message) {
+        super(message);
+    }
+
+}
